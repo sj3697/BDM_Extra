0e94e2dc8fbfa728ca8bc7b80ac4be86474fa411
59418 <90524952+sj3697@users.noreply.github.com>
2022-05-14 12:06:25 -0700

    Add files via upload

--- final.py
+++ final.py
@@ -50,7 +50,7 @@ def main(sc,sqlcontext):
       df2_2019_03 = df2_2019_03.groupBy('cbg').agg(med.alias('2019_03')).cache()
 
       
-      final.write.format("csv").option("header", "true").save(sys.argv[1])
+      df2_2019_03.write.format("csv").option("header", "true").save(sys.argv[1])
 if __name__ == '__main__':
   sc = SparkContext()
   sqlcontext=SQLContext(sc)
